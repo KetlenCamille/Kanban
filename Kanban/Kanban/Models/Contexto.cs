3d2180e399932fc9c4248638085198cb76736382
KetlenCamille <ketlen.rieper@gmail.com>
2019-04-19 17:45:34 -0300

    Criando Contexto e SingletonContext

---
+++ Kanban/Kanban/Models/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Kanban.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("DbKanban") { }
+
+        public DbSet<Status> Status { get; set; }
+        public DbSet<Atividade> Atividades { get; set; }
+    }
+}
---
+++ Kanban/Kanban/Models/SingletonContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kanban.Models
+{
+    public class SingletonContext
+    {
+        private static Contexto ctx;
+        private SingletonContext() { }
+
+        public static Contexto GetInstance()
+        {
+            if (ctx == null)
+            {
+                ctx = new Contexto();
+            }
+            return ctx;
+        }
+    }
+}
