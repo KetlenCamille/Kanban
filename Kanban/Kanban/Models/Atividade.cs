3db107370b62ccd87ef23f40c044a66c2d396e5e
KetlenCamille <ketlen.rieper@gmail.com>
2019-04-19 16:16:50 -0300

    Instalando EntityFramework. Criando as Models Status e Atividade

---
+++ Kanban/Kanban/Models/Atividade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Kanban.Models
+{
+    [Table("Atividades")]
+    public class Atividade
+    {
+        [Key]
+        public int IDAtividade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [Display(Name = "Título da atividade")]
+        public String TituloAtividade { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [Display(Name = "Descrição da atividade")]
+        public String DescricaoAtividade { get; set; }
+
+        [Display(Name = "Requisito da atividade")]
+        public int RequisitoAtividade { get; set; }
+
+        [Display(Name = "Status da atividade")]
+        public Status Status { get; set; }
+    }
+}
---
+++ Kanban/Kanban/Models/Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Kanban.Models
+{
+    [Table("Status")]
+    public class Status
+    {
+        [Key]
+        public int IDStatus { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [Display(Name = "Descrição da categoria")]
+        public String DescricaoStatus { get; set; }
+    }
+}
