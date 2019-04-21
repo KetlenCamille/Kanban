df0198529347ad05597cd8fbf4cf359779e811a8
KetlenCamille <ketlen.rieper@gmail.com>
2019-04-20 23:29:13 -0300

    Criação do StatusDAO.

---
+++ Kanban/Kanban/DAL/StatusDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kanban.Models;
+
+namespace Kanban.DAL
+{
+    public class StatusDAO
+    {
+        private static Contexto contexto = SingletonContext.GetInstance();
+
+        public void Adicionar(Status status)
+        {
+            if (status != null)
+            {
+                contexto.Status.Add(status);
+                contexto.SaveChanges();
+            }
+        }
+
+        public List<Status> ListarTodos()
+        {
+            return contexto.Status.ToList();
+        }
+
+        public void Atualizar(Status status)
+        {
+            contexto.Entry(status).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public void Remover(int id)
+        {
+            contexto.Status.Remove(BuscarPorID(id));
+            contexto.SaveChanges();
+        }
+
+        public Status BuscarPorID(int? id)
+        {
+            return contexto.Status.Find(id);
+        }
+
+        public Status BuscarPorNome(string status)
+        {
+            return contexto.Status.FirstOrDefault(x => x.DescricaoStatus.ToLower().Contains(status.ToLower()));
+        }
+    }
+    c 
+    }
+}
